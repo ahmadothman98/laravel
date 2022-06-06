9ceec4585103697b1266381ff4b27b896c9c47f5
Ahmad Othman <aothman156@gmail.com>
2022-06-06 20:04:13 +0300

    random nominee implemented

--- app/Http/Controllers/nomineeController.php
+++ app/Http/Controllers/nomineeController.php
@@ -2,6 +2,14 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
-class  extends Controller{
-    public function
+class nomineeController extends Controller{
+    public function nominee(){
+    $student_list = array("ahmad","ali","nour","mohammad","samah","pablo");
+    $rand_int = rand(0,count($student_list)-1);
+    return response() ->json([
+        "message" => "success",
+        "nominee" => $student_list[$rand_int]
+    ],200);
+
+    }
 }
