1080241e3fa2a5a547decb7985effe5a57e2b19a
Ahmad Othman <aothman156@gmail.com>
2022-06-06 19:56:38 +0300

    student groups implemented

--- app/Http/Controllers/groupController.php
+++ app/Http/Controllers/groupController.php
@@ -2,6 +2,17 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
-class  extends Controller{
-    public function
-}
+class groupController extends Controller{
+    public function groupOfTwo(){
+        $first_student = '';
+        $second_student = '';
+        $student_list = array("ahmad","ali","nour","mohammad","samah");
+        shuffle($student_list);
+        $groups = array_chunk($student_list,2);
+        return response() ->json([
+            "message" => "success",
+            "student groups" => $groups
+        ],200);
+
+        }
+    }
